00d8f9edcc1c1197e305919e93551dced57fce9f
Nathan Brown <nathanejbrown@gmail.com>
2023-10-13 12:58:04 -0600

    testing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,8 +8,7 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    loadChildren: () => import ('./home/home.module').then(m => m.HomeModule)
   }
 ];
 
