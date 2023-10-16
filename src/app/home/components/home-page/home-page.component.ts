cc5b56a0c07755c3f5cea22ddddca5155070bb9e
Nathan Brown <nathanejbrown@gmail.com>
2023-10-16 14:06:16 -0600

    redo major styles of home page

--- src/app/home/components/home-page/home-page.component.ts
+++ src/app/home/components/home-page/home-page.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent {
-
+  donationText: string = "I offer free nipple and areola tattoos to anyone in need. Every human deserves to feel whole, empowered and confident. Many people have had their femininity and/or sexuality stripped away by tragedy, in addition to their finances being drained. I will not profit from their suffering, I only want to offer some semblance of their former reality, so they aren't constantly reminded of their loss when they look in the mirror. Please consider a donation so I can continue to provide love and light to those who need it. Your donation will cause a ripple into the universe, compounding positive energy all around you. Thank YOU!"
 }
